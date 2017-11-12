2f2882c3fa6f1c46e2a1c7d72da05e911c302577
Peter Alvaro <palvaro@gmail.com>
2017-11-12 14:53:46 -0800

    flush

--- parser/parser_test.py
+++ parser/parser_test.py
@@ -1,9 +1,13 @@
 import tatsu
 from tatsu.ast import AST
 from tatsu.walkers import NodeWalker
+from tatsu.model import ModelBuilderSemantics
 from pprint import pprint
 import unittest
 from dedalus import TrivialSemantics, DedalusSemantics
+from codegen import C4CodeGenerator
+
+from negprov import NegNodeWalker
 
 
 class GrammarTest(unittest.TestCase):
@@ -12,39 +16,42 @@ class GrammarTest(unittest.TestCase):
         self.grammar = open('dedalus.tatsu').read()
         self.parser = tatsu.compile(self.grammar)
 
+        self.grammar_asmodel = open('dedalus_asmodel.tatsu').read()
+        self.parser_asmodel = tatsu.compile(self.grammar_asmodel, asmodel=True)
 
-    def test_simple(self):
+
+    def Ntest_simple(self):
         prog = 'log(Node, Pload)@next :- log(Node, Pload);'
         ast = self.parser.parse(prog, trace=False, colorize=True, semantics=TrivialSemantics())
         self.assertEqual(str(ast), prog)
 
-    def test_simple2(self):
+    def Ntest_simple2(self):
         prog = 'log(Node, Pload)@next :- log(Node, Pload), frog(Node);'
         ast = self.parser.parse(prog, trace=False, colorize=True, semantics=TrivialSemantics())
         self.assertEqual(str(ast), prog)
 
-    def test_simple3(self):
+    def Ntest_simple3(self):
         prog = 'log(Node, Pload)@next :- log(Node, Pload), notin frog(Node);'
         ast = self.parser.parse(prog, trace=False, colorize=True, semantics=TrivialSemantics())
         self.assertEqual(str(ast), prog)
 
 
-    def test_simple4(self):
+    def Ntest_simple4(self):
         prog = 'log(Node, Pload + 1)@next :- log(Node, Pload), frog(Node);'
         ast = self.parser.parse(prog, trace=False, colorize=True, semantics=TrivialSemantics())
         self.assertEqual(str(ast), prog)
 
-    def test_simple5(self):
+    def Ntest_simple5(self):
         prog = "a(X, Y) :- b(X, Z), c(Z, Y), d(Y, W);"
         ast = self.parser.parse(prog, trace=False, colorize=True, semantics=TrivialSemantics())
         self.assertEqual(str(ast), prog)
     
-    def test_assign(self):
+    def Ntest_assign(self):
         prog = "a(X, Y, A) :- b(X, Z), c(Z, Y), d(Y, W), A = Y / Z;"
         ast = self.parser.parse(prog, trace=False, colorize=True, semantics=TrivialSemantics())
         self.assertEqual(str(ast), prog)
     
-    def test_2pc(self):
+    def Ntest_2pc(self):
         prog = """include "./2pc_edb.ded";
 prepare(Agent, Coord, Xact)@async :- running(Coord, Xact), agent(Coord, Agent);"""
         ast = self.parser.parse(prog, trace=False, colorize=True, semantics=TrivialSemantics())
@@ -56,12 +63,29 @@ prepare(Agent, Coord, Xact)@async :- running(Coord, Xact), agent(Coord, Agent);"
         ast = self.parser.parse(prog, trace=False, colorize=True, semantics=DedalusSemantics())
         print "AST " + str(ast)
 
-    def test_fact(self):
+    def Ntest_fact(self):
         prog = 'bcast("a", 1)@1;';
         ast = self.parser.parse(prog, trace=False, colorize=True, semantics=TrivialSemantics())
         self.assertEqual(str(ast), prog)
 
-    def test_qual(self):
+    def test_negation(self):
+        print "TEST NEGATION"
+        #prog = 'log(Node, Pload)@next :- log(Node, Pload), notin frog(Node); frog(Node)@next :- bob(Node);'
+        prog = 'log(Node, Pload)@next :- log(Node, Pload), notin frog(Node); frog(Node) :- bob(Node);'
+        model = self.parser_asmodel.parse(prog)
+        walker = NegNodeWalker()
+        w = walker.walk(model)
+        #print "walked " + str(model)
+        print "walked2 " + str(w)
+
+        
+
+        #w.to_dot().render("FOO")
+        text = C4CodeGenerator().render(model)
+
+        print "TEXT " + text
+
+    def Ntest_qual(self):
         prog = 'a(X, Y) :- b(X, Y), Y != X;';
         ast = self.parser.parse(prog, trace=False, colorize=True, semantics=TrivialSemantics())
         self.assertEqual(str(ast), prog)
