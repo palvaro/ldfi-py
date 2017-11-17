b6e1a717c2d49243b52fe36ae1ebb30fb0e1a819
Peter Alvaro <palvaro@gmail.com>
2017-11-17 13:39:02 -0800

    flush

--- parser/codegen.py
+++ parser/codegen.py
@@ -6,16 +6,30 @@ THIS_MODULE =  sys.modules[__name__]
 
 class C4CodeGenerator(CodeGenerator):
     def __init__(self):
+        print "YO"
         super(C4CodeGenerator, self).__init__(modules=[THIS_MODULE])
 
-    #def rule(ModelRenderer):
-    #    template = '''\
-    #    {lhs} :- {rhs}'''
-
-    def program(ModelRenderer):
+class rule(ModelRenderer):
+        print "RULE"
         template = '''\
-        FOO'''
+        {lhs} :- {rhs}'''
 
+class program(ModelRenderer):
+        print "PRTO"
+        template = '''\
+        {rules}'''
+
+class stmt(ModelRenderer):
+    template = "safd"
+
+class statement(ModelRenderer):
+    template = "ya motha"
+
+class stmlist(ModelRenderer):
+        print "UH"
+        #print "UM " + str(stmts)
+        template = '''\
+        S:{stmts}'''
 
 
 
